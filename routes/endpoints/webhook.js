1531aaf776b598bcd7e815f5dbafbb6b7e4532a9
Nick <nick.spriet@student.howest.be>
2015-12-31 18:36:38 +0100

    read changed field webhook

--- routes/endpoints/webhook.js
+++ routes/endpoints/webhook.js
@@ -1,6 +1,7 @@
 /**
  * Created by Nick Spriet on 09/12/2015.
  */
+var async = require('async');
 
 //TODO: export keys to config file
 exports.testje = function (req, res) {
@@ -19,6 +20,21 @@ exports.testje = function (req, res) {
 
 
 exports.addEvent = function (req, res) {
-    console.log("request fb:", req);
-    console.log("response fb:", res);
+    var userId = req.body.entry[0].id;
+    var changedFields = req.body.entry[0].changed_fields;
+
+    async.forEach(changedFields, function (item, cb) {
+        switch (item) {
+            case 'events':
+            //update events for user with userId
+
+        }
+
+        // tell async that the iterator has completed
+        cb();
+
+    }, function (err) {
+        console.log('iterating done');
+    });
+
 };
