2030f6735471a94410ecf1b3247685e128ecbdde
Nick <nick.spriet@student.howest.be>
2016-01-03 00:09:13 +0100

    delete event from backend

--- data/models/Event.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Nick Spriet on 29/11/2015.
- */
-var mongoose = require('mongoose');
-var EventSchema = require('../schemas/EventSchema');
-
-var Event = mongoose.model('Event', EventSchema);
-module.exports = Event;
